e3b5d60af8299e8b987a56af9b18bef07224706a
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-11-11 17:57:24 -0300

    Criando um novo componente no Angular

--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -1,12 +1,16 @@
+// Arquivo central para aplicação, para mapea- los na aplicação
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent
   ],
   imports: [
     BrowserModule,
