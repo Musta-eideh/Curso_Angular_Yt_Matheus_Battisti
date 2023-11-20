0116ffc16ea556a6ee2477639e7d3cc5ae908e33
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-11-20 19:59:35 -0300

    Interpolando dados dos componentes

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit{
+  name: string = "Mustafa";
+  age: number = 30;
+  job = "programmer";
+  hobbies = ["Correr", "Jogar", "Estudar"];
+  car = {
+    name: "Pollo",
+    year: 2019
+  }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
